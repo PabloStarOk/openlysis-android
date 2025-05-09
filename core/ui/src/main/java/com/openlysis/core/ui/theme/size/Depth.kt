447e3181400a295a99cc3ee0a1f8bd515c99f684
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-05-08 20:16:54 -0500

    feat: add spacing and depth values for styling

---
+++ core/ui/src/main/java/com/openlysis/core/ui/theme/size/Depth.kt
@@ -0,0 +1,13 @@
+package com.openlysis.core.ui.theme.size
+
+import androidx.compose.ui.unit.dp
+
+object Depth {
+    val Value0 = 0.dp
+    val Value025 = 1.dp
+    val Value100 = 4.dp
+    val Value200 = 8.dp
+    val Value400 = 16.dp
+    val Value800 = 32.dp
+    val Value1200 = 48.dp
+}
---
+++ core/ui/src/main/java/com/openlysis/core/ui/theme/size/Spacing.kt
@@ -0,0 +1,19 @@
+package com.openlysis.core.ui.theme.size
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val Value0 = 0.dp
+    val Value050 = 2.dp
+    val Value100 = 4.dp
+    val Value150 = 6.dp
+    val Value200 = 8.dp
+    val Value300 = 12.dp
+    val Value400 = 16.dp
+    val Value600 = 24.dp
+    val Value800 = 32.dp
+    val Value1200 = 48.dp
+    val Value1600 = 64.dp
+    val Value2400 = 96.dp
+    val Value4000 = 160.dp
+}
