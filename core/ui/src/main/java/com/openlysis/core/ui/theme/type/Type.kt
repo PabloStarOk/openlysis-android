de241b0e6cf7900a76e35d5a67c19204ef751cbb
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-05-08 21:34:33 -0500

    feat: add montserrat font family

--- core/ui/src/main/java/com/openlysis/core/ui/theme/type/Type.kt
+++ core/ui/src/main/java/com/openlysis/core/ui/theme/type/Type.kt
@@ -1,58 +1,91 @@
 package com.openlysis.core.ui.theme.type
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.openlysis.core.ui.R
+
+@OptIn(ExperimentalTextApi::class)
+private val Montserrat =
+    FontFamily(
+        Font(
+            resId = R.font.montserrat_variable_font_wght,
+            weight = FontWeight.Bold,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(700)
+                )
+        ),
+        Font(
+            resId = R.font.montserrat_variable_font_wght,
+            weight = FontWeight.SemiBold,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(600)
+                )
+        ),
+        Font(
+            resId = R.font.montserrat_variable_font_wght,
+            weight = FontWeight.Normal,
+            variationSettings =
+                FontVariation.Settings(
+                    FontVariation.weight(400)
+                )
+        )
+    )
 
 // From Figma
 object Title {
     val Hero =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Bold,
             fontSize = Scales.Value10,
             lineHeight = 86.4.sp
         )
     val Level1 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Bold,
             fontSize = Scales.Value08,
             lineHeight = 57.6.sp
         )
     val Level2 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Bold,
             fontSize = Scales.Value07,
             lineHeight = 48.sp
         )
     val Level3 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Bold,
             fontSize = Scales.Value06,
             lineHeight = 38.4.sp
         )
     val Level4 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Bold,
             fontSize = Scales.Value05,
             lineHeight = 28.8.sp
         )
     val Level5 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.SemiBold,
             fontSize = Scales.Value04,
             lineHeight = 24.sp
         )
     val Level6 =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.SemiBold,
             fontSize = Scales.Value03,
             lineHeight = 19.2.sp
@@ -62,7 +95,7 @@ object Title {
 object Body {
     val Large =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Normal,
             fontSize = Scales.Value04,
             lineHeight = 28.sp
@@ -71,7 +104,7 @@ object Body {
 
     val Base =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Normal,
             fontSize = Scales.Value03,
             lineHeight = 22.4.sp
@@ -80,7 +113,7 @@ object Body {
 
     val Small =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Normal,
             fontSize = Scales.Value02,
             lineHeight = 19.6.sp
@@ -89,7 +122,7 @@ object Body {
 
     val ExtraSmall =
         TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = Montserrat,
             fontWeight = FontWeight.Normal,
             fontSize = Scales.Value01
         )
