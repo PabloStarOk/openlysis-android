9f2e31476f915b9319aeb14d6a827aadba4c02b8
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-05-09 16:30:37 -0500

    feat: add strong base to label and colors on pressed for tool card

--- feature/tools/src/main/java/com/openlysis/feature/tools/components/ToolCard.kt
+++ feature/tools/src/main/java/com/openlysis/feature/tools/components/ToolCard.kt
@@ -1,6 +1,10 @@
 package com.openlysis.feature.tools.components
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -18,13 +22,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.openlysis.core.ui.theme.OpenlysisTheme
 import com.openlysis.core.ui.theme.size.Spacing
+import com.openlysis.core.ui.theme.type.Body
 
 @Composable
 internal fun ToolCard(
@@ -35,19 +44,44 @@ internal fun ToolCard(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed = interactionSource.collectIsPressedAsState()
+
+    val containerColor by
+        GetColorState(
+            isPressed = isPressed,
+            defaultColor = MaterialTheme.colorScheme.background,
+            pressedColor = MaterialTheme.colorScheme.surface
+        )
+
+    val borderColor by
+        GetColorState(
+            isPressed = isPressed,
+            defaultColor = MaterialTheme.colorScheme.outline,
+            pressedColor = Color.Unspecified
+        )
+
+    val labelColor by
+        GetColorState(
+            isPressed = isPressed,
+            defaultColor = MaterialTheme.colorScheme.onBackground,
+            pressedColor = MaterialTheme.colorScheme.onSurface
+        )
+
     Card(
         modifier =
             modifier
                 .height(IntrinsicSize.Min),
         colors =
             CardColors(
-                containerColor = MaterialTheme.colorScheme.background,
+                containerColor = containerColor,
                 contentColor = MaterialTheme.colorScheme.onBackground,
                 disabledContainerColor = MaterialTheme.colorScheme.background,
                 disabledContentColor = MaterialTheme.colorScheme.onBackground
             ),
+        border = BorderStroke(1.dp, borderColor),
         onClick = onClick,
-        border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline)
+        interactionSource = interactionSource
     ) {
         Row(
             modifier =
@@ -68,7 +102,8 @@ internal fun ToolCard(
             ) {
                 Text(
                     text = label,
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = Body.BaseStrong,
+                    color = labelColor,
                     modifier = Modifier.fillMaxWidth()
                 )
                 Text(
@@ -87,6 +122,17 @@ internal fun ToolCard(
         }
     }
 }
+
+@Composable
+private fun GetColorState(
+    isPressed: State<Boolean>,
+    defaultColor: Color,
+    pressedColor: Color
+): State<Color> =
+    animateColorAsState(
+        if (isPressed.value) pressedColor else defaultColor,
+        animationSpec = tween(durationMillis = 200)
+    )
 
 @Preview(showSystemUi = true)
 @Composable
