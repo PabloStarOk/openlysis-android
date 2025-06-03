173cfbeb102b524175ea6b3604210215e5daee92
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-06-02 22:39:52 -0500

    refactor(core): standardize organization of design system's typography

--- feature/tools/src/main/java/com/openlysis/feature/tools/components/ToolCard.kt
+++ feature/tools/src/main/java/com/openlysis/feature/tools/components/ToolCard.kt
@@ -33,7 +33,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.openlysis.core.designsystem.theme.OpenlysisTheme
 import com.openlysis.core.designsystem.theme.size.Spacing
-import com.openlysis.core.designsystem.theme.type.LocalTypography
+import com.openlysis.core.designsystem.theme.type.LocalAppTypography
 
 @Composable
 internal fun ToolCard(
@@ -102,7 +102,7 @@ internal fun ToolCard(
             ) {
                 Text(
                     text = label,
-                    style = LocalTypography.current.bodyBaseStrong,
+                    style = LocalAppTypography.current.bodyBaseStrong,
                     color = labelColor,
                     modifier = Modifier.fillMaxWidth()
                 )
