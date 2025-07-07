93c25128f94d1c191079d8ca34b30e490fd5b316
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-07-07 13:50:02 -0500

    feat(data): remove pagination settings to improve UI flexibility

--- core/data/analysis/core/src/main/java/com/openlysis/data/analysis/core/di/AnalysisProvidingModule.kt
+++ core/data/analysis/core/src/main/java/com/openlysis/data/analysis/core/di/AnalysisProvidingModule.kt
@@ -1,7 +1,6 @@
 package com.openlysis.data.analysis.core.di
 
 import com.openlysis.data.analysis.core.repository.AnalysesRepository
-import com.openlysis.data.analysis.core.repository.AnalysesRepositorySettings
 import com.openlysis.data.analysis.core.repository.DefaultAnalysesRepository
 import com.openlysis.data.analysis.core.request.AnalyzeMessage
 import com.openlysis.data.analysis.core.source.AnalysesLocalDataSource
@@ -24,13 +23,11 @@ internal object AnalysisProvidingModule {
     @Singleton
     @Provides
     fun provideEmailAnalysesRepository(
-        settings: AnalysesRepositorySettings,
         localDs: AnalysesLocalDataSource<MessageAnalysis>,
         @EmailAnalysesRemoteDataSource remoteDs:
             AnalysesRemoteDataSource<AnalyzeMessage, MessageAnalysis>
     ): AnalysesRepository<AnalyzeMessage, MessageAnalysis> =
         DefaultAnalysesRepository(
-            settings = settings,
             localDs = localDs,
             remoteDs = remoteDs
         )
@@ -39,13 +36,11 @@ internal object AnalysisProvidingModule {
     @Singleton
     @Provides
     fun provideSmsAnalysesRepository(
-        settings: AnalysesRepositorySettings,
         localDs: AnalysesLocalDataSource<MessageAnalysis>,
         @SmsAnalysesRemoteDataSource remoteDs:
             AnalysesRemoteDataSource<AnalyzeMessage, MessageAnalysis>
     ): AnalysesRepository<AnalyzeMessage, MessageAnalysis> =
         DefaultAnalysesRepository(
-            settings = settings,
             localDs = localDs,
             remoteDs = remoteDs
         )
