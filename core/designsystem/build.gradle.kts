aba89e2ccae1d72d04ba0de5b899e837b139ceb0
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-07-10 19:15:28 -0500

    build: add compose BOM for version management and remove unnecessary dependencies
    - Add compose BOM as implementation to manage versions of compose libraries.
    - Remove version of material3 library as it is now managed by the BOM.
    - Remove unnecessary dependencies that are not used in the project.

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -34,9 +34,9 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation)
--- feature/tools/build.gradle.kts
+++ feature/tools/build.gradle.kts
@@ -37,9 +37,9 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation)
