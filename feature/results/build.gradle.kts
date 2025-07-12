416c1773e319eaf80e02586d565b535424a18790
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-07-11 22:22:52 -0500

    build(results): update module's gradle to improve version compatibility using bom dependency

--- feature/results/build.gradle.kts
+++ feature/results/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.navigation)
