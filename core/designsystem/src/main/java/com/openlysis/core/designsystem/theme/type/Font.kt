a8a4e6f1e6f830ed22eda36a64095c007f63fbb1
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-07-24 21:05:33 -0500

    feat(core): change fonts from variable to static for 24 API level compatibility

--- core/designsystem/src/main/java/com/openlysis/core/designsystem/theme/type/Font.kt
+++ core/designsystem/src/main/java/com/openlysis/core/designsystem/theme/type/Font.kt
@@ -1,31 +1,25 @@
 package com.openlysis.core.designsystem.theme.type
 
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import com.openlysis.core.designsystem.R
 
 /**
  * Defines the Montserrat font family.
  */
-@OptIn(ExperimentalTextApi::class)
 internal val Montserrat =
     FontFamily(
         Font(
-            resId = R.font.montserrat_variable_font_wght,
-            weight = FontWeight.Bold,
-            variationSettings = FontVariation.Settings(FontVariation.weight(700))
+            resId = R.font.montserrat_bold,
+            weight = FontWeight.Bold
         ),
         Font(
-            resId = R.font.montserrat_variable_font_wght,
-            weight = FontWeight.SemiBold,
-            variationSettings = FontVariation.Settings(FontVariation.weight(600))
+            resId = R.font.montserrat_semibold,
+            weight = FontWeight.SemiBold
         ),
         Font(
-            resId = R.font.montserrat_variable_font_wght,
-            weight = FontWeight.Normal,
-            variationSettings = FontVariation.Settings(FontVariation.weight(400))
+            resId = R.font.montserrat,
+            weight = FontWeight.Normal
         )
     )
