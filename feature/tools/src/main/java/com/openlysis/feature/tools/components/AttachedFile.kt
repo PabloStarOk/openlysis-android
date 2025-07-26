36d35c687ebf4ea8ad2773db0a2dd0225ed6679d
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-07-25 20:54:34 -0500

    feat(tools): set KeyboardType to password for AttachedFile's password input field

--- feature/tools/src/main/java/com/openlysis/feature/tools/components/AttachedFile.kt
+++ feature/tools/src/main/java/com/openlysis/feature/tools/components/AttachedFile.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
@@ -132,7 +134,8 @@ internal fun AttachedFile(
                     iconAlt = togglePasswordTransformationIconAlt
                 )
             },
-            visualTransformation = inputVisualTransformation
+            visualTransformation = inputVisualTransformation,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
     }
 }
