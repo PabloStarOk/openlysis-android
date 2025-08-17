2698cc407b572666aa411707f25dc4e79ca9156e
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-08-17 17:17:47 -0500

    feat(app): set app's theme based on user theme configuration

--- app/src/main/java/com/openlysis/MainActivityUiState.kt
+++ app/src/main/java/com/openlysis/MainActivityUiState.kt
@@ -1,5 +1,6 @@
 package com.openlysis
 
+import com.openlysis.data.user.model.ThemeConfig
 import com.openlysis.navigation.AppPermission
 
 /**
@@ -10,12 +11,27 @@ internal sealed interface MainActivityUiState {
      * Success UI state.
      *
      * @property userSignedIn Indicates if the user is signed in.
+     * @property userThemeConfig The user's selected theme configuration.
      * @property pendingPermissions Permissions pending to be asked to the user.
      */
     data class Success(
         val userSignedIn: Boolean,
+        var userThemeConfig: ThemeConfig,
         val pendingPermissions: List<AppPermission>
-    ) : MainActivityUiState
+    ) : MainActivityUiState {
+        /**
+         * Determines if the dark theme should be used based on the user's theme configuration.
+         *
+         * @param isSystemInDarkTheme Indicates if the system is currently in dark theme.
+         * @return true if the user's theme config is dark, false if light, or matches the system setting.
+         */
+        override fun shouldUseDarkTheme(isSystemInDarkTheme: Boolean) =
+            when (userThemeConfig) {
+                ThemeConfig.System -> isSystemInDarkTheme
+                ThemeConfig.Light -> false
+                ThemeConfig.Dark -> true
+            }
+    }
 
     /**
      * UI state when loading is in progress.
@@ -35,4 +51,12 @@ internal sealed interface MainActivityUiState {
      * @return true if the current state is Loading, false otherwise.
      */
     fun shouldKeepSplashScreen() = this is Loading
+
+    /**
+     * Determines if the dark theme should be used.
+     *
+     * @param isSystemInDarkTheme Indicates if the system is currently in dark theme.
+     * @return true if the dark theme should be used, false otherwise.
+     */
+    fun shouldUseDarkTheme(isSystemInDarkTheme: Boolean) = isSystemInDarkTheme
 }
