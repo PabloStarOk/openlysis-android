a3c73834ea8ab94452e076789c912fca24729959
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-08-17 17:54:52 -0500

    feat(design-system): switch device's status and nav bar themes when app's theme changes

--- core/designsystem/src/main/java/com/openlysis/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/openlysis/core/designsystem/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.openlysis.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.openlysis.core.designsystem.theme.color.DarkAppColorScheme
 import com.openlysis.core.designsystem.theme.color.LightAppColorScheme
 import com.openlysis.core.designsystem.theme.radius.FallbackRadius
@@ -25,6 +29,16 @@ fun OpenlysisTheme(
             darkTheme -> DarkAppColorScheme
             else -> LightAppColorScheme
         }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insetsController = WindowCompat.getInsetsController(window, view)
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+            insetsController.isAppearanceLightNavigationBars = !darkTheme
+        }
+    }
 
     CompositionLocalProvider(
         LocalAppColorScheme provides appColorScheme,
