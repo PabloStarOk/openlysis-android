92bf1a9259850311d220ef1e52f475cfcf8face0
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2025-08-17 20:49:26 -0500

    fix: add rules to keep enum classes after minification

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,3 +1,5 @@
+-keep class com.openlysis.data.analysis.model.message.MessageType { *; }
+
 -assumenosideeffects class android.util.Log {
     public static boolean isLoggable(java.lang.String, int);
     public static int v(...);
--- feature/auth/src/main/java/com/openlysis/feature/auth/model/AuthenticationType.kt
+++ feature/auth/src/main/java/com/openlysis/feature/auth/model/AuthenticationType.kt
@@ -1,11 +1,14 @@
 package com.openlysis.feature.auth.model
 
+import androidx.annotation.Keep
+
 /**
  * Represents the type of authentication action.
  *
  * - SignIn: User is signing in.
  * - SignUp: User is registering a new account.
  */
+@Keep
 enum class AuthenticationType {
     SignIn,
     SignUp
